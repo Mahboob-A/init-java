a0f6413c6cb8b0935f0a262869ed874b584a595b
Mahboob-A <iammahboob.a@gmail.com>
2025-06-13 08:36:26 +0530

    add: order of operation

---
+++ operators-if-else-num-system/OperatorIfElseNumSystem/src/AssignmentOperators/OrderOfOperation.java
@@ -0,0 +1,13 @@
+package AssignmentOperators;
+
+public class OrderOfOperation {
+    public static void main(String[] args) {
+        // BODMAS or PEMDAS rule
+
+
+        // if multiply types of operators are present, then use BODMAS rule.
+        // if same type of many operators, then execute from left.
+
+
+    }
+}
